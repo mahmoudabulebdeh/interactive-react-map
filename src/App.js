196596949c8a9c65167cf73dc66f84d97426b7e2
Mahmoud <m.a.uae@live.com>
2019-08-04 00:00:27 +0100

    Adding Map Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Main from './components/MainComponent';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 
 const App = () => (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-
+import SplytMap from './SplytMapComponent';
 class Main extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
         <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-2 " />
-            <div className="col-12 col-md-8" />
-            <div className="col-12 col-md-2" />
+          <div className="d-flex justify-content-center" id="map">
+            <SplytMap />
           </div>
         </div>
         <Footer />
